91d33a4baad99ee13ae026fa8ef5a2fece3ba843
skaroupka <skaroupka.kuba@gmail.com>
2021-09-28 17:00:16 +0200

    added example

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 #this is the main file
 def convert(seconds):
      seconds = seconds % (24 * 3600)
+     hour = seconds // 3600
+     seconds %= 3600
+     minutes = seconds // 60
+     seconds %= 60
+
      return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+ n = 12345
+ print(convert(n))
